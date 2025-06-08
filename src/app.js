cdeee0f84632b7551fdcfd6f8a294e59a5a31729
José Isaac <joseisaacnascimento@gmail.com>
2025-06-07 21:39:38 -0300

    feat: Cria o form de cadastro - Cria a página index.html - Cria rota raiz (\) para exibir o index.html

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 // import express
 import express from "express";
+// importa o path
+import path from "path";
 // instancia o express
 const app = express();
 // importa o arquivo de conexão
@@ -11,17 +13,45 @@ app.use(express.json());
 // para aceitar dados do form
 app.use(express.urlencoded({ extended: true }));
 // para servir arquivos estáticos
-app.use(express.static("public"));
+app.use(express.static("../public"));
+
+// importa o dirname e o fileURLToPath
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+// define o __dirname numa variável
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // rotas
+// Rota principal
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const indexPath = path.join(__dirname + "/../public/index.html");
+  res.sendFile(indexPath);
 });
 
 // rota para listar os clientes
 app.get("/clientes", (req, res) => {
   const sql = "select * from clientes";
   conexao.query(sql, (err, data) => {
+    if (err) throw err;
+    res.json(data);
+  });
+});
+
+// rota para pegar um cliente pelo id
+app.get("/clientes/:id", (req, res) => {
+  const sql = "select * from clientes where id = ?";
+  conexao.query(sql, [req.params.id], (err, data) => {
+    if (err) throw err;
+    res.json(data);
+  });
+});
+
+// rota para cadastrar um cliente
+app.post("/clientes", (req, res) => {
+  const cliente = req.body;
+  const sql = "insert into clientes set ?";
+  conexao.query(sql, [cliente], (err, data) => {
     if (err) throw err;
     res.json(data);
   });
