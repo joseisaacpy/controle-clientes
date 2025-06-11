8373c78a27838593d47bb2ec7333e5364b13af0e
José Isaac <joseisaacnascimento@gmail.com>
2025-06-10 21:36:55 -0300

    feat: Verifica se há pelo menos algum cliente cadastrado e mostra mensagem caso não haja nenhum

--- public/js/clientes-cadastrados.js
+++ public/js/clientes-cadastrados.js
@@ -7,6 +7,7 @@ async function listarClientes() {
   ul.innerHTML = "";
   if (data.length === 0) {
     const li = document.createElement("li");
+    li.textContent = "Nenhum cliente cadastrado.";
     ul.appendChild(li);
     return;
   }
